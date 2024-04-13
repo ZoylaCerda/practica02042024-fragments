5e2952205a3adafb5e797c6f86b468b4c981c7a7
Marvin Ramos <mr487223@gmail.com>
2024-04-13 13:16:51 -0600

    add fragments to nav menu

--- app/src/main/java/sv/edu/ufg/fis/amb/ramosaragon/cuarto_fragment.kt
+++ app/src/main/java/sv/edu/ufg/fis/amb/ramosaragon/cuarto_fragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [segundo_fragmento.newInstance] factory method to
+ * Use the [cuarto_fragmento.newInstance] factory method to
  * create an instance of this fragment.
  */
-class segundo_fragmento : Fragment() {
+class cuarto_fragmento : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class segundo_fragmento : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_segundo_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_cuarto_fragment, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class segundo_fragmento : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment segundo_fragmento.
+         * @return A new instance of fragment cuarto_fragmento.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            segundo_fragmento().apply {
+            cuarto_fragmento().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
